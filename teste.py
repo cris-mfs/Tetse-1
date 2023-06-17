8ce4aa5a790b1e5ac3e3e4eb916bbffafd00438e
Cristiano Silva <134558808+cris-mfs@users.noreply.github.com>
2023-06-18 00:51:36 +0100

    Código com jogo operacional
    Pode ser melhor a nivel da comunicação com o jogador e na elaboração do boneco

--- teste.py
+++ teste.py
@@ -1,34 +1,77 @@
 # Teste 1
+
+def boneco(numero):
+    resto = 5 - numero
+    if numero == 1:
+        boneco = """
+        o
+        
+        
+            """ 
+        print(boneco)
+        print("Tente novamente. Restam-lhe {} tentativas!".format(resto))   
+    elif numero == 2:
+        boneco = """
+        \o/
+         
+        
+            """ 
+        print(boneco)
+        print("Tente novamente. Restam-lhe {} tentativas!".format(resto))   
+    elif numero == 3:
+        boneco = """
+        \o/
+         |
+        
+            """ 
+        print(boneco)
+        print("Tente novamente. Restam-lhe {} tentativa!".format(resto))  
+    elif numero == 4:
+        boneco = """
+        \o/
+         |
+        /
+            """ 
+        print(boneco)
+        print("Tente novamente. Restam-lhe {} tentativas!".format(resto))
+    elif numero == 5:
+        boneco = """
+        \o/
+         |
+        / |
+            """ 
+        print(boneco)
+        print("Lamento, você perdeu")
+    return
+
 import random
-#contador para tentativas falahadas
 
 lista_palavras = ["batata","gnar","mialgia"]
 palavra_secreta = random.sample(lista_palavras,1)[0].lower()
 
-#palavra_secreta.lower()
-#print("A palavra secreta é: ", palavra_secreta)
-
 # Mostrar palavra secreta com espaços em branco
-resposta_list = ""
+resposta_str = ""
 for i in range(len(palavra_secreta)):
-    resposta_list = resposta_list + "_"
-resposta_list = list(resposta_list)
-print(resposta_list)
+    resposta_str = resposta_str + "_"
+resposta_list = list(resposta_str)
+print("Aqui vai uma dica! A palavra secreta tem {} letras!\n{}".format(len(palavra_secreta),resposta_str))
 
-# número tentativas função para faze ro boneco consoante ov alor do numero de tentativas 
-# só dar errado se for uma tentiva falhada
+i=0
 falha = 0
+historico_tentativas = []
 while falha < 5:
-    tentativa = str(input("escreva uma letra: ")).lower()
-    i = 0
-    for letra in palavra_secreta:
+    tentativa = str(input("Escreva uma letra: ")).lower() #
+    tentativa = tentativa[0] # caso o utilizador escreva mais que uma letra, apenas utilizar a primeira letra
+    resposta_old = "".join(resposta_list) # cria a variavel que vai utilizar para considerar uma tantativa errada
+    for letra in palavra_secreta: # loop para mostrar a letra  certa
         if tentativa == letra:
             resposta_list[i]=letra
         i = i + 1
     resposta_str = "".join(resposta_list)
-    print(resposta_str)
+    print("valor de resposta actual", resposta_str)
+    if resposta_old == resposta_str:
+        falha = falha + 1
+        boneco(falha)
     if resposta_str == palavra_secreta:
         print("Parabéns, Ganhou!!")
         break
-
-# sair quando o utilizador acertar na
